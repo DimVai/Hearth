a7fbc0ef7fe2bb28aa5e3d976ca7aa668ea0bf33
Dimitris Vainanidis <dvainanidis@gmail.com>
2026-05-04 15:30:07 +0300

    1.2 - Minor style

--- public/js/add-connection.js
+++ public/js/add-connection.js
@@ -8,9 +8,10 @@ Q('#add-form').on('submit', function(e) {
     const name = Q('#name').element.value.trim();
     const communicationFrequencyDays = Number(Q('#frequency').element.value);
     const lastCommunicationDate = Q('#last-communication').element.value || null;
+    const scheduledNextCommunicationDate = Q('#scheduled-next').element.value || null;
 
     if (!name || !communicationFrequencyDays) return;
 
-    network.addConnection({ name, communicationFrequencyDays, lastCommunicationDate });
+    network.addConnection({ name, communicationFrequencyDays, lastCommunicationDate, scheduledNextCommunicationDate });
     window.location.href = '../index.html';
 });
